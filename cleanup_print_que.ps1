783759de394545e1a1ce969563be835751a57099
alazare619 <alazare619@gmail.com>
2015-11-18 10:26:40 -0600

    Adjust Variables and Spacing

--- cleanup_print_que.ps1
+++ cleanup_print_que.ps1
@@ -1,14 +1,18 @@
-# change age to the amount of days old the file has to be.
+# Age the file must be to be deleted minimum of 1 day
 $age = 1
-# Change path to folder to monitor full path necessary I.E. C:\Windows\System32\spool\PRINTERS
+# Path to print que folder
 $path = "C:\Windows\System32\spool\PRINTERS"
-#Leave Alone
+# Function to get date of file
 $limit = (Get-Date).AddDays(-$age)
 
+# Stop the Printer Service
 Stop-Service spooler
+
 # Delete files older than the $limit.
 Get-ChildItem -Path $path -Recurse -Force | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $limit } | Remove-Item -Force
 
 # Delete any empty directories left behind after deleting the old files.
 Get-ChildItem -Path $path -Recurse -Force | Where-Object { $_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | Where-Object { !$_.PSIsContainer }) -eq $null } | Remove-Item -Force -Recurse
+
+# Start the Printer Service
 Start-Service spooler
