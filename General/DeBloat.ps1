e1add70122fbecd4cb9abcb37908372598676eaf
alazare619 <alazare619@gmail.com>
2015-11-19 17:03:43 -0600

    Add Export CSV Verbose Information

--- General/DeBloat.ps1
+++ General/DeBloat.ps1
@@ -1,15 +1,20 @@
-#############Variables#####################
+#############Editable Variables#####################
 
 $TSTAMP = Get-Date -format "dd-MMM-yyyy_HH-mm-ss"
 $LOGPATH = "C:\logs\debloat"
-$LOGFILE = "debloat_%TSTAMP%.log"
-$ProgramsRemoved = @()
+$LOGPATH = "\\server\share\debloat"
+$LOGFILE = "debloat_$TSTAMP.log"
+$LOGFILE = "debloat.log"
 $Dl_Programs_By_GUID = "https://raw.githubusercontent.com/bmrf/tron/master/resources/stage_2_de-bloat/programs_to_target_by_GUID.bat"
 $Dl_Programs_By_NAME = "https://raw.githubusercontent.com/bmrf/tron/master/resources/stage_2_de-bloat/programs_to_target_by_name.txt"
 $Dl_Toolbars_By_GUID = "https://raw.githubusercontent.com/bmrf/tron/master/resources/stage_2_de-bloat/toolbars_BHOs_to_target_by_GUID.bat"
+
+
+############Phase0 Initialization Do Not Edit Below#######################
+$ProgramsRemoved = @()
 $Programs = Get-CIMInstance -Classname Win32_Product
 $InstalledSoftware = $Programs | Select -Expandproperty "Identifyingnumber"
-
+mkdir $LogPath -ea SilentlyContinue
 
 
 ############Phase1 Removal By GUID#######################
@@ -34,23 +39,24 @@ $BadGUIDs = $GUIDVOCprogs + $GUIDVOCToolbars | Select -Unique
 $InstalledBadGUIDs = $InstalledSoftware | Compare $BadGUIDs -Excludedifferent -Includeequal| Select -Expandproperty Inputobject
 $InstalledBadGUIDs | Foreach {
 Start-Process "Msiexec.Exe" -Argumentlist "/Qn","/Norestart","/X","$_" -Wait
-$ProgramsRemoved += $Programs -match $_ | Select -ExpandProperty Name
+$ProgramsRemoved += $Programs -match $_ | Select Name,IdentifyingNumber
 }
 
 
 ###########Phase2 Removal Using WildCard Names#########################
 
-## Job: Remove Crapware Programs, Phase 2: Wildcard By Name
 ## Search Through The List Of Programs In "Programs_To_Target_Name.Txt" File And Uninstall Them One-By-One
-$Webrequest = ((Iwr $Dl_Programs_By_NAME).Tostring() -Replace "%%","*").Split([Environment]::Newline) | where {$_} #Like Above, This Clears Out Empty Lines. Like At The End Of A File :)
-Foreach ($Item In $Webrequest) { 
-  If ($Uninstalling = $Programs | Where {$_.Name -Like $Item})
+$WebrequestStringArray = ((Iwr $Dl_Programs_By_NAME).Tostring() -Replace "%%","*").Split([Environment]::Newline) | where {$_} 
+Foreach ($String In $WebrequestStringArray) { 
+  If ($Uninstalling = $Programs | Where {$_.Name -Like $String})
   {
-    $Uninstalling | Invoke-Cimmethod -Methodname Uninstall -Whatif  #Send All Programs Through Pipe. Find All Objects That Have A Name Property That Matches One Of The $Item In $Webrequest And Uninstall It
-    $ProgramsRemoved += $Programs -match $uninstalling.IdentifyingNumber | Select -ExpandProperty Name #Hmmm, can I match on this object? Might be able to!
+    $Uninstalling | Invoke-Cimmethod -Methodname Uninstall -Whatif  
+    $ProgramsRemoved += $Programs -match $uninstalling.IdentifyingNumber | Select Name,IdentifyingNumber
   }
 }
 
 
 ###########Phase3 Logging the removed programs#########################
-$ProgramsRemoved | Out-File $LogPath\$LogFile
+$ProgramsRemoved | Add-Member -MemberType NoteProperty -Name "Hostname" -Value $Env:COMPUTERNAME
+$ProgramsRemoved | Add-Member -MemberType NoteProperty -Name "TimeStamp" -Value $TSTAMP
+$ProgramsRemoved | select Hostname, Timestamp, Name, IdentifyingNumber | Export-Csv -append $LogPath\$LogFile -NoTypeInformation
